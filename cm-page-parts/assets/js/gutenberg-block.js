ad4733cfb9416721b63c67112dcfe9ee9acea3b7
Estel Smith <estel.smith@gmail.com>
2020-02-05 16:51:51 -0500

    make page part block use a dropdown containing part names

--- cm-page-parts/assets/js/gutenberg-block.js
+++ cm-page-parts/assets/js/gutenberg-block.js
@@ -1,8 +1,26 @@
 (function (api, blockEditor, blocks, components, element) {
     let el = element.createElement;
-    let PlainText = blockEditor.PlainText;
     let Dashicon = components.Dashicon;
     let SelectControl = components.SelectControl;
+
+    let collectionEndpoint = new api.collections.Cm_page_part();
+    let selectOptions = [];
+    collectionEndpoint.fetch().done(function (pageCollection) {
+        selectOptions.push({
+            label: '-- Select --',
+            value: null
+        });
+
+        for (let pageIndex in pageCollection) {
+            if (pageCollection.hasOwnProperty(pageIndex)) {
+                let page = pageCollection[pageIndex];
+                selectOptions.push({
+                    label: page.title.rendered,
+                    value: page.id
+                });
+            }
+        }
+    });
 
     blocks.registerBlockType(
         'cm/page-part',
@@ -13,6 +31,9 @@
             category: 'widgets',
             attributes: {
                 id: {
+                    type: 'number'
+                },
+                title: {
                     type: 'string'
                 }
             },
@@ -23,39 +44,43 @@
             },
             edit: function (props) {
                 let id = 'cm_page_part_' + Math.random().toString(36).substr(2, 9);
+                if (!props.attributes.title) {
+                    props.attributes.title = '-- Select --';
+                }
+
+                let emptySelect = [{
+                    label: props.attributes.title,
+                    value: null
+                }];
 
                 let selectElement = el(
                     SelectControl,
                     {
                         id: id,
-                        // value: 'test2',
-                        options: [
-                            {label: 'Test 1', value: 'test1'},
-                            {label: 'Test 2', value: 'test2'},
-                        ]
-                    }
-                );
+                        value: props.attributes.id,
+                        options: selectOptions.length !== 0 ? selectOptions : emptySelect,
+                        onChange: function (value) {
+                            value = parseInt(value);
+                            if (Number.isNaN(value)) value = null;
+                            let title = '';
 
-                let collectionEndpoint = new api.collections.Cm_page_part();
-                let selectOptions = [];
-                collectionEndpoint.fetch({data:{fields: 'id,title'}}).done(function (pageCollection) {
-                    for (let pageIndex in pageCollection) {
-                        if (pageCollection.hasOwnProperty(pageIndex)) {
-                            let page = pageCollection[pageIndex];
-                            selectOptions.push({
-                                label: page.title.rendered,
-                                value: page.id
+                            for (let selectIndex in selectOptions) {
+                                if (selectOptions.hasOwnProperty(selectIndex)) {
+                                    let selectOption = selectOptions[selectIndex];
+                                    if (selectOption.value === value) {
+                                        title = selectOption.label;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            props.setAttributes({
+                                id: value,
+                                title: title
                             });
                         }
                     }
-
-                    // (function () {
-                    //     this.props.setAttributes({options: selectOptions});
-                    // }).bind(selectElement)();
-                    // selectElement.props.setAttributes({options: selectOptions});
-                    // console.log(selectElement);
-                    //@TODO figure out how to dynamically alter select options.
-                });
+                );
 
                 return el(
                     'div',
@@ -76,19 +101,7 @@
                         ),
                         'Page Part'
                     ),
-                    // selectElement
-                    el(
-                        PlainText,
-                        {
-                            className: 'input-control',
-                            id: id,
-                            placeholder: 'Put ID here...',
-                            value: props.attributes.id,
-                            onChange: function (value) {
-                                props.setAttributes({id: value});
-                            }
-                        }
-                    )
+                    selectElement
                 );
             },
             save: function () {
