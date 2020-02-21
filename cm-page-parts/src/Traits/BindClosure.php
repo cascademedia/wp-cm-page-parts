2f3672df3086c6567c6d8c792d5667fd3226edba
Estel Smith <estel.smith@gmail.com>
2020-02-21 17:06:37 -0500

    deprecate BindClosure, since it's actually not needed

--- cm-page-parts/src/Traits/BindClosure.php
+++ cm-page-parts/src/Traits/BindClosure.php
@@ -4,6 +4,12 @@ namespace CascadeMedia\WordPress\PageParts\Traits;
 
 trait BindClosure
 {
+    /**
+     * @deprecated Turns out PHP 7.x scopes Closures to the class they were defined in.
+     *
+     * @param \Closure $closure
+     * @return \Closure
+     */
     protected function bindThis(\Closure $closure)
     {
         return \Closure::bind($closure, $this, $this);
